7ab1f8e56469eed6e4eaa7a90089ffbbfb52274a
Kapej <kacper.trafial@live.com>
2015-07-06 23:15:28 +0200

    Implementacja bardziej zaawansowanego wyszukiwanua

---
+++ app/models/search.rb
@@ -0,0 +1,15 @@
+class Search < ActiveRecord::Base
+
+  def search_books
+
+   books = Book.all
+
+   books = books.where("name like ?", "%#{keywords}%") if keywords.present?
+   books = books.where("category like ?", cetegory) if cetegory.present?
+   books = books.where("price >= ?", min_price) if min_price.present?
+   books = books.where("price <= ?", max_price) if max_price.present?
+
+   return books
+end
+
+end
